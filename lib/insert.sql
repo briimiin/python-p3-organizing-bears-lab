391aafa62fcd964575ad9bec899f22b96bc65edf
briimiin <kipkorirbrimin005@gmail.com>
2024-06-02 16:34:54 +0300

    organising bears lab

---
+++ lib/insert.sql
@@ -0,0 +1,9 @@
+INSERT INTO bears (name, sex) VALUES
+('Mr. Chocolate', 'M'),
+('Rowdy', 'F'),
+('Tabitha', 'F'),
+('Sergeant Brown', 'M'),
+('Melissa', 'M'),
+('Grinch', 'M'),
+('Wendy', 'M'),
+(NULL, 'F');
